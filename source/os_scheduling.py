1ebd858357348f1f198dac9024acd5107ff7d70f
jesus <sandoval.j4@outlook.com>
2023-11-13 21:32:09 -0800

    EDF is COMPLETED! and stuff

--- source/os_scheduling.py
+++ source/os_scheduling.py
@@ -33,7 +33,9 @@ if __name__ == '__main__':
         edf_table = []
         ex_table = []
         temp_table = []
-        total_energy = [0, 0, 0, 0, 0]
+        total_energy = 0
+        total_idle = 0
+        total_execution = 0
         ex_time = 0
         index = 0
         index_check = 0
@@ -61,7 +63,6 @@ if __name__ == '__main__':
                 min_table = []
                 for i in edf_table:
                     min_table.append(i - clock)
-                # print("Preemption: ", min_table)
                 index = min_table.index(min(min_table))
                 index_check = index
                 print(clock+1, " ", end="")
@@ -100,60 +101,57 @@ if __name__ == '__main__':
                             edf_table[hold] = temp_table[hold]
                             it_flag[hold] = 0
                             index = 10
-                            # print("\nCLOCK", clock)
-                            # print("\nEDF", edf_table)
-                            # print("\nTEMP", temp_table)
-                            # print("\nITERATIONS", iterations)
-                            # print("DSAHFGBAERYIGFOL", hold)
 
                 if state == 1:
                     print(ex_time, " ", end="")
                     print(round(i_cpu_idle * ex_time * 0.001, 2), "J")
+                    total_energy += round(i_cpu_idle * ex_time * 0.001, 2)
+                    total_idle += ex_time
                 else:
                     print(ex_table[index_check], " ", end="")
                     print(round(i_cpu_high * ex_table[index_check] * 0.001, 2), "J")
-                # print(edf_table)
-                # print(min_table)
+                    total_energy += round(i_cpu_high * ex_time * 0.001, 2)
+            print("Total Energy Consumption: ", total_energy, "J")
+            print("Percentage Time spent IDLE: ", total_idle, "seconds")
+            print("Total Execution Time: ", clock, "seconds")
 
-                # END
+    if sys.argv[2] == "RM":
+        # Finding Earliest Deadline First at maximum CPU Frequency #
+        row_count = len(text_input)
+        table = []
+        iterations = []
+        it_flag = []
+        edf_table = []
+        ex_table = []
+        temp_table = []
+        total_energy = 0
+        total_idle = 0
+        total_execution = 0
+        ex_time = 0
+        index = 0
+        index_check = 0
+        index_prev = 3
+        clock = 0
+        state = 0
+        # Create EDF Table
+        for i in range(row_count - 1):
+            table.append(text_input.iloc[i + 1])
+        for i in range(len(table)):
+            edf_table.append(table[i][1])
+        for i in range(len(table)):
+            ex_table.append(table[i][2])
+        for i in range(len(table)):
+            iterations.append(1)
+        for i in range(len(table)):
+            temp_table.append(table[i][1])
+        for i in range(len(table)):
+            it_flag.append(1)
 
-                # Updating EDF & Execution Tables
-                # ex_table[index_check] = table[index_check][2]
-                # iterations[index_prev] += 1
-                # edf_table[index_check] = (iterations[index_check] * table[index_check][1])
-                # state += 1
-                # print("State", state)
-                # print("EDF", edf_table)
-                # print("EX", ex_table)
-                # exit()
-                # clock += 1
+        # EDF Scheduler
+        while clock < 1000:
 
-            # Indexing Earliest Deadline
-            # print(edf_table.index(max(edf_table)))
-            """
-            for i in range(row_count-1):
-                table.append(text_input.iloc[i+1])
+            exit()
 
-            for i in range(len(table)):
-                min_table = sorted(table, key=lambda x: x[1])
-            
-            # EDF Scheduler
-            clock = 1
-            print("Earliest Deadline at Maximum CPU Frequency: ")
-            # Getting Total Energy Used and Total Execution
-            for i in range(len(min_table)):
-                total_energy[i] = cpu_high * min_table[i][2] * 0.001
-            # Printing Schedule Results
-            for i in range(len(min_table)):
-                print(clock, min_table[i][0], i_cpu_high, min_table[i][2],
-                      total_energy[i], "J")
-                # Keeping track of the clock
-                clock += min_table[i][2]
-
-        print("Total Energy Consumption: ", sum(total_energy), "J")
-        print("Percentage Time spent IDLE: 0 seconds")
-        print("Total Execution Time: ", clock-1, "seconds")
-        """
     if len(sys.argv) == 4:
         # Finding Earliest Deadline and Energy Efficiency #
         row_count = len(text_input)
